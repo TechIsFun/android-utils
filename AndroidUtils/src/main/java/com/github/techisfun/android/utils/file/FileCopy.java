e0eeaf0f8e9f5ce579fc3d899b5728573f8332af
andreamaglie <andrea.maglie@misiedo.com>
2014-02-24 16:40:02 +0100

    file copy support class

---
+++ AndroidUtils/src/main/java/com/github/techisfun/android/utils/file/FileCopy.java
@@ -0,0 +1,44 @@
+package com.github.techisfun.android.utils.file;
+
+import java.io.*;
+import java.nio.channels.FileChannel;
+
+/**
+ * Created by Andrea Maglie on 24/02/14.
+ */
+public class FileCopy {
+
+    /**
+     * Creates the specified toFile as a byte for byte copy of the
+     * fromFile. If toFile already exists, then it
+     * will be replaced with a copy of fromFile. The name and path
+     * of toFile will be that of toFile.
+     *
+     *  Note: fromFile and toFile will be closed by
+     * this function.
+     *
+     * @param fromFile
+     *            - FileInputStream for the file to copy from.
+     * @param toFile
+     *            - FileInputStream for the file to copy to.
+     */
+    public void copy(FileInputStream fromFile, FileOutputStream toFile) throws IOException {
+        FileChannel fromChannel = null;
+        FileChannel toChannel = null;
+        try {
+            fromChannel = fromFile.getChannel();
+            toChannel = toFile.getChannel();
+            fromChannel.transferTo(0, fromChannel.size(), toChannel);
+        } finally {
+            try {
+                if (fromChannel != null) {
+                    fromChannel.close();
+                }
+            } finally {
+                if (toChannel != null) {
+                    toChannel.close();
+                }
+            }
+        }
+    }
+}
